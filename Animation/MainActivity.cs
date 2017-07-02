9e2fd458665a84ebe1aa45097988a85accb50c91
ali2 <ali62n62@yahoo.com>
2017-07-02 14:50:35 +0430

    init method added

--- Animation/MainActivity.cs
+++ Animation/MainActivity.cs
@@ -13,6 +13,12 @@ namespace Animation
 
             // Set our view from the "main" layout resource
              SetContentView (Resource.Layout.Main);
+            initFields();
+        }
+
+        private void initFields()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
