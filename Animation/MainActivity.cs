e4ca7fa13bed08c3cd76c338db1205e8538648c7
ali2 <ali62n62@yahoo.com>
2017-07-03 12:02:27 +0430

    animate bart position

--- Animation/MainActivity.cs
+++ Animation/MainActivity.cs
@@ -9,7 +9,7 @@ namespace Animation
     public class MainActivity : Activity
     {
         private ImageView _imageViewBart;
-        private ImageView _imageViewHomer;
+        
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -25,25 +25,15 @@ namespace Animation
             _imageViewBart = FindViewById<ImageView>(Resource.Id.imageViewBart);
             _imageViewBart.Click += fadeBart;
 
-            _imageViewHomer = FindViewById<ImageView>(Resource.Id.imageViewHomer);
-            _imageViewHomer.Click += fadeHomer;
+          
         }
 
-        private void fadeHomer(object sender, System.EventArgs e)
-        {
-
-            _imageViewBart.Animate().Alpha(1.0f).SetDuration(2000);
-            _imageViewHomer.Animate().Alpha(0f).SetDuration(2000);
-            _imageViewBart.TranslationZ = 10;
-            _imageViewHomer.TranslationZ = 0;
-        }
+      
 
         private void fadeBart(object sender, System.EventArgs e)
         {
-            _imageViewBart.Animate().Alpha(0f).SetDuration(2000);
-            _imageViewHomer.Animate().Alpha(1.0f).SetDuration(2000);
-            _imageViewBart.TranslationZ = 0;
-            _imageViewHomer.TranslationZ = 10;
+            _imageViewBart.Animate().XBy(-100f).SetDuration(2000);
+            _imageViewBart.Animate().YBy(100f).SetDuration(2000);
         }
     }
 }
