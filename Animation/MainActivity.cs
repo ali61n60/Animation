1194e2acb7c460b0ffcdc5047a7dc915fa39f19f
ali2 <ali62n62@yahoo.com>
2017-07-03 14:36:43 +0430

    animationset

--- Animation/MainActivity.cs
+++ Animation/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using Android.Util;
+using Android.Views.Animations;
 using Java.Lang;
 
 namespace Animation
@@ -28,12 +29,34 @@ namespace Animation
         }
 
 
-        private int _numberOfRotation = 0;
+        
         private void fadeBart(object sender, System.EventArgs e)
         {
-            _numberOfRotation++;
-            _imageViewBart.Animate().Rotation(45f*_numberOfRotation).SetDuration(500);
+            AnimationSet animationSet=new AnimationSet(false);
+            long duration =300;
+            animationSet.AddAnimation(createAnimation(1f,0.5f,1f,0.5f,duration,0));
+            animationSet.AddAnimation(createAnimation(1f,4f, 1f, 4f,duration,1*duration));
+            animationSet.AddAnimation(createAnimation(1f, 0.5f, 1f, 0.5f,duration, 2*duration));
+
+            animationSet.FillAfter = true;
+
+            // scale(0.5f,1000,0);
+            //scale(2f,1000,1000);
+            //scale(1f,1000,2000);
+
+            _imageViewBart.StartAnimation(animationSet);
         }
+
+        private ScaleAnimation createAnimation(float x0,float x1,float y0,float y1,long duration,long startOffset)
+        {
+            ScaleAnimation scaleAnimation = new ScaleAnimation(x0,x1,y0,y1);
+            scaleAnimation.Duration = duration;
+            scaleAnimation.StartOffset = startOffset;
+            scaleAnimation.FillAfter = true;
+            return scaleAnimation;
+        }
+
+
     }
 }
 
